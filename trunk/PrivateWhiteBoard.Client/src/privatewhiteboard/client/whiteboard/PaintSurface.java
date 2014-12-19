5ca5f72dee85c7d31b3407c1483c0bed8642b6ca
buithimaibtm@gmail.com <buithimaibtm@gmail.com@cb853672-4dc1-ba39-2469-877e928f095b>
2014-12-19 06:55:07 +0000

    Add whiteboard

--- trunk/PrivateWhiteBoard.Client/src/privatewhiteboard/client/whiteboard/PaintSurface.java
+++ trunk/PrivateWhiteBoard.Client/src/privatewhiteboard/client/whiteboard/PaintSurface.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package privatewhiteboard.client;
+package privatewhiteboard.client.whiteboard;
 
 import java.awt.AlphaComposite;
 import java.awt.BasicStroke;
