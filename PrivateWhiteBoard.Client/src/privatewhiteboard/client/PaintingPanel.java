3881edcd06e296b9f2bfef8da39ed7f647ad00fe
buithimaibtm <buithimaibtm@gmail.com>
2014-12-19 06:51:31 +0000

    Add whiteboard

---
+++ PrivateWhiteBoard.Client/src/privatewhiteboard/client/PaintingPanel.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package privatewhiteboard.client;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Bui Thi Mai
+ */
+public class PaintingPanel extends JPanel{
+    private javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JList jlPaintOptions;
+    PaintSurface paint = new PaintSurface();
+    
+    public PaintingPanel(){
+        Initialize();
+    }
+    
+    private void Initialize(){
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints contraint = new GridBagConstraints();
+        contraint.fill = GridBagConstraints.HORIZONTAL;
+        
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jlPaintOptions = new javax.swing.JList();
+        
+        jlPaintOptions.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jlPaintOptions.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { " Pointer", " FreeHand", " Line  ", " Rectangle", " Ellipse", " Text", " Erase", " Hand" };
+            @Override
+            public int getSize() { return strings.length; }
+            @Override
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jlPaintOptions.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jlPaintOptions.setLayoutOrientation(javax.swing.JList.VERTICAL_WRAP);
+        jlPaintOptions.setSelectedIndex(1);
+        jlPaintOptions.setVisibleRowCount(1);
+        jScrollPane1.setViewportView(jlPaintOptions);
+        
+        //add list paint options
+        contraint.weightx = 0.5;
+        contraint.weighty = 0.5;
+        contraint.gridx = 0;
+        contraint.gridy = 0;
+        contraint.anchor = GridBagConstraints.BASELINE_LEADING;
+        this.add(jScrollPane1, contraint);
+        
+        //add paint to panel
+        contraint.ipady = 400;
+        contraint.weightx = 0.5;
+        contraint.weighty = 0.5;
+        contraint.gridx = 0;
+        contraint.gridy = 1;
+        contraint.anchor = GridBagConstraints.ABOVE_BASELINE_LEADING;
+        this.add(paint, contraint);
+        
+    }
+}
+
