4429c069d16fcf4bc04d1f07f8a6c2236de796e3
buithimaibtm@gmail.com <buithimaibtm@gmail.com@cb853672-4dc1-ba39-2469-877e928f095b>
2014-12-19 06:51:31 +0000

    Add whiteboard

---
+++ trunk/PrivateWhiteBoard.Client/src/privatewhiteboard/client/ShapeTypes.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package privatewhiteboard.client;
+
+/**
+ *
+ * @author Bui Thi Mai
+ */
+public enum ShapeTypes {
+    ELLIPE,
+    RECTANGLE,
+    LINE,
+    NONE
+}
