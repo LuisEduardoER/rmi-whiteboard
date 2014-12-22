4e05b26dc68abdcf02e9f8121d0687d9f3a1af97
buithimaibtm@gmail.com <buithimaibtm@gmail.com@cb853672-4dc1-ba39-2469-877e928f095b>
2014-12-22 08:49:06 +0000

    Update whireboard

--- trunk/PrivateWhiteBoard.Client/src/privatewhiteboard/client/whiteboard/PaintingPanel.java
+++ trunk/PrivateWhiteBoard.Client/src/privatewhiteboard/client/whiteboard/PaintingPanel.java
@@ -9,7 +9,11 @@ package privatewhiteboard.client.whiteboard;
 import privatewhiteboard.client.whiteboard.PaintSurface;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Rectangle;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import privatewhiteboard.shared.controllers.IClientController;
 
 /**
@@ -19,8 +23,9 @@ import privatewhiteboard.shared.controllers.IClientController;
 public class PaintingPanel extends JPanel{
     private javax.swing.JScrollPane jScrollPane1;
     public javax.swing.JList jlPaintOptions;
-    PaintSurface paint = new PaintSurface();
-    
+    private PaintSurface paint;
+    private JScrollPane paintScrollPanel;
+    private ColorChooser colorChooser;
     IClientController _clientController;
     
     public PaintingPanel(){
@@ -32,22 +37,37 @@ public class PaintingPanel extends JPanel{
         this.setLayout(new GridBagLayout());
         GridBagConstraints contraint = new GridBagConstraints();
         contraint.fill = GridBagConstraints.HORIZONTAL;
-        
+
         jScrollPane1 = new javax.swing.JScrollPane();
         jlPaintOptions = new javax.swing.JList();
+        paintScrollPanel = new JScrollPane();
+        colorChooser = new ColorChooser();
+        paint = new PaintSurface(this);
         
+        paint.fillColor = colorChooser.getCurrentColor();
+        
+        paintScrollPanel.scrollRectToVisible(new Rectangle(0, 0, 400, 400));
+
+        //jlPaintOptions set up
         jlPaintOptions.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jlPaintOptions.setModel(new javax.swing.AbstractListModel() {
-            String[] strings = { " Pointer", " FreeHand", " Line  ", " Rectangle", " Ellipse", " Text", " Erase", " Hand" };
+            String[] strings = {" Pointer", " FreeHand", " Line  ", " Rectangle", " Ellipse", " Text", " Eraser", " Hand"};
+
             @Override
-            public int getSize() { return strings.length; }
+            public int getSize() {
+                return strings.length;
+            }
+
             @Override
-            public Object getElementAt(int i) { return strings[i]; }
+            public Object getElementAt(int i) {
+                return strings[i];
+            }
         });
         jlPaintOptions.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         jlPaintOptions.setLayoutOrientation(javax.swing.JList.VERTICAL_WRAP);
         jlPaintOptions.setSelectedIndex(1);
         jlPaintOptions.setVisibleRowCount(1);
+        jlPaintOptions.addListSelectionListener(new PaintOptionListener());
         jScrollPane1.setViewportView(jlPaintOptions);
         
         //add list paint options
@@ -58,15 +78,104 @@ public class PaintingPanel extends JPanel{
         contraint.anchor = GridBagConstraints.BASELINE_LEADING;
         this.add(jScrollPane1, contraint);
         
+        
+        contraint.gridx = 0;
+        contraint.gridy = 2;
+        contraint.ipadx = 5;
+        contraint.anchor = GridBagConstraints.ABOVE_BASELINE_LEADING;
+        this.add(colorChooser, contraint);
+        
+
         //add paint to panel
         contraint.ipady = 400;
         contraint.weightx = 0.5;
         contraint.weighty = 0.5;
         contraint.gridx = 0;
-        contraint.gridy = 1;
+        contraint.gridy = 3;
         contraint.anchor = GridBagConstraints.ABOVE_BASELINE_LEADING;
-        this.add(paint, contraint);
+
+        paintScrollPanel.setViewportView(paint);
+
+        this.add(paintScrollPanel, contraint);
         
+    }
+    
+    private PaintOptions getOption(int index) {
+        PaintOptions result = PaintOptions.DRAW;
+        switch (index) {
+            case 0:
+                result = PaintOptions.POINTER;
+                break;
+            case 1:
+                paint.shapeType =  getShapeType(index);
+                result = PaintOptions.DRAW;
+                break;
+            case 2:
+                paint.shapeType =  getShapeType(index);
+                result = PaintOptions.DRAW;
+                break;
+            case 3:
+                paint.shapeType =  getShapeType(index);
+                result = PaintOptions.DRAW;
+                break;
+            case 4:
+                paint.shapeType =  getShapeType(index);
+                result = PaintOptions.DRAW;
+                break;
+            case 5:
+                paint.shapeType =  getShapeType(index);
+                result = PaintOptions.DRAW;
+                break;
+            case 6:
+                result = PaintOptions.ERASER;
+                break;
+            case 7:
+                result = PaintOptions.HAND;
+                break;
+            default:
+                result = PaintOptions.DRAW;
+                break;
+        }
+        return result;
+    }
+
+    private ShapeTypes getShapeType(int index) {
+        ShapeTypes result = ShapeTypes.RECTANGLE;
+        switch (index) {
+            case 1:
+                result = ShapeTypes.NONE;
+                break;
+            case 2:
+                result = ShapeTypes.LINE;
+                break;
+            case 3:
+                result = ShapeTypes.RECTANGLE;
+                break;
+            case 4:
+                result = ShapeTypes.ELLIPE;
+                break;
+            case 5:
+                result = ShapeTypes.TEXT;
+                break;
+            default:
+                result = ShapeTypes.RECTANGLE;
+                break;
+        }
+
+        return result;
+    }
+
+    public ColorChooser getColorChooser() {
+        return colorChooser;
+    }
+
+    //Event controler
+    private class PaintOptionListener implements ListSelectionListener {
+
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            paint.option = getOption(jlPaintOptions.getSelectedIndex());
+        }
     }
 }
 
